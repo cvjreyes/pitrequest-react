1afdc65b833a4ff52cb505a536e7e7c4e8fe3983
Sean <sean.saez.f@gmail.com>
2022-08-02 09:33:27 +0200

    Imagen PitRequest te lleva al menu principal

--- src/components/menuListPit/menuListPit.js
+++ src/components/menuListPit/menuListPit.js
@@ -4,9 +4,9 @@ import './menuList.css'
 import PITLogo from "../../assets/images/pitlogo.svg"
 
 
-
-
 const MenuListPIT = (props) =>{
+
+    const [goMainMenu, setGoMainMenu] = useState(false)
 
     function success(){
         props.success()
@@ -16,21 +16,28 @@ const MenuListPIT = (props) =>{
         props.successProject()
     }
 
+    function mainMenu(){
+        setGoMainMenu(!goMainMenu)
+    }    
+
     return(
+
         <div class="panel__container">
+
             <div className="panel__heading__container">
-                <img src={PITLogo} alt="PITLogo" className="pit__image"/>
+                <img src={PITLogo} alt="PITLogo" className="pit__image" onClick={()=>mainMenu()}/>
             </div>
 
             <div className="elements__container">
+
                 <div className="menu" style={{paddingTop:"10px"}}>
-                    <MenuListPITList success={success.bind(this)} successProject={successProject.bind(this)}/>
+                    <MenuListPITList success={success.bind(this)} successProject={successProject.bind(this)} goMainMenu={goMainMenu}/>
                 </div>
                 
             </div>
             
-            
         </div>
+        
     );
 };
 
